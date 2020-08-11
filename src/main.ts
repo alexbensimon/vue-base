273d4de8388af188ac6671650216068de59753b2
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2020-08-11 14:50:13 +0200

    Prettier all files

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './assets/tailwind.css'
-import { createProvider } from './vue-apollo'
+import Vue from "vue";
+import App from "./App.vue";
+import "./assets/tailwind.css";
+import { createProvider } from "./vue-apollo";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   apolloProvider: createProvider(),
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
